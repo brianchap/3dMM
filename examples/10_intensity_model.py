01c12be8d4ebf028592ef6cb6bba96b5c7290989
Chandra Suresh <chandra.b.suresh@gmail.com>
2021-01-07 19:08:00 -0800

    Prelim data generation

--- examples/10_intensity_model.py
+++ examples/10_intensity_model.py
@@ -20,13 +20,13 @@ import numpy as np
 import scipy.io as sio
 
 # width of beckmann microfacet distribution
-beck_width = 0.000001
+beck_width = 0.35
 
 # Refractive index of the material the light is coming from
-eta_i = 0.5
+eta_i = 1
 
 # Refractive index of the material the light is hitting/entering
-eta_t = 0.5
+eta_t = 1.45
 
 # Microfacet distribution function
 def distr(w_h):
@@ -420,121 +420,127 @@ triangles = bfm.triangles
 #global colors
 #global triangles
 #colors = C['colors']; triangles = C['triangles']
-colors = colors/np.max(colors)
-source = (400, 100, 50)
-strength_val, angles, normals = compute_strength(550, 0.045, 0.22, vertices, triangles, source)
-print('\n' + "Bounds Check")
-print(np.percentile(strength_val, 1))
-print(np.percentile(strength_val, 100))
-#print(np.mean(strength_val))
-#print(np.std(strength_val))
-#strength_val = strength_val - (np.mean(strength_val) - 2*np.std(strength_val))
-#strength_val = ((1/4) / np.std(strength_val)) * strength_val
-#strength_val = (strength_val - np.percentile(strength_val, 1))*1/(np.percentile(strength_val, 100) - np.percentile(strength_val, 1))
-#for index, sing_val in enumerate(strength_val):
-#	if strength_val[index] < 0:
-#		strength_val[index] = 0
-#	elif strength_val[index] > 1:
-#		strength_val[index] = 0.9999999999
-print("-----")
-#for index, sing_val in enumerate(strength_val):
-#	if strength_val[index] < np.percentile(strength_val, 20):
-#		strength_val[index] = 0
-#	elif strength_val[index] < np.percentile(strength_val, 40):
-#		strength_val[index] = 0.25
-#	elif strength_val[index] < np.percentile(strength_val, 60):
-#		strength_val[index] = 0.5
-#	elif strength_val[index] < np.percentile(strength_val, 80):
-#		strength_val[index] = 0.75
-#	else:
-#		strength_val[index] = 1
-#print(strength_val)
-#strength_val = np.log(strength_val) + 1
-#for index, sing_val in enumerate(strength_val):
-#	if strength_val[index] < 0:
-#		strength_val[index] = 0
-vertices = vertices - np.mean(vertices, 0)[np.newaxis, :]
+orig_source = [400, 100, 50]
+for cam_ang in [-25, 25]:
+    for i in range(3):
+        source = orig_source.copy()
+        source[i] += cam_ang
+        source = tuple(source)
+        colors = colors/np.max(colors)
 
-# save folder
-save_folder = 'results/intensity_distro'
-if not os.path.exists(save_folder):
-    os.mkdir(save_folder)
-options = '-delay 10 -loop 0 -layers optimize' # gif options. need ImageMagick installed.
+        strength_val, angles, normals = compute_strength(550, 0.045, 0.22, vertices, triangles, source)
+        print('\n' + "Bounds Check")
+        print(np.percentile(strength_val, 1))
+        print(np.percentile(strength_val, 100))
+        #print(np.mean(strength_val))
+        #print(np.std(strength_val))
+        #strength_val = strength_val - (np.mean(strength_val) - 2*np.std(strength_val))
+        #strength_val = ((1/4) / np.std(strength_val)) * strength_val
+        #strength_val = (strength_val - np.percentile(strength_val, 1))*1/(np.percentile(strength_val, 100) - np.percentile(strength_val, 1))
+        #for index, sing_val in enumerate(strength_val):
+        #	if strength_val[index] < 0:
+        #		strength_val[index] = 0
+        #	elif strength_val[index] > 1:
+        #		strength_val[index] = 0.9999999999
+        print("-----")
+        #for index, sing_val in enumerate(strength_val):
+        #	if strength_val[index] < np.percentile(strength_val, 20):
+        #		strength_val[index] = 0
+        #	elif strength_val[index] < np.percentile(strength_val, 40):
+        #		strength_val[index] = 0.25
+        #	elif strength_val[index] < np.percentile(strength_val, 60):
+        #		strength_val[index] = 0.5
+        #	elif strength_val[index] < np.percentile(strength_val, 80):
+        #		strength_val[index] = 0.75
+        #	else:
+        #		strength_val[index] = 1
+        #print(strength_val)
+        #strength_val = np.log(strength_val) + 1
+        #for index, sing_val in enumerate(strength_val):
+        #	if strength_val[index] < 0:
+        #		strength_val[index] = 0
+        vertices = vertices - np.mean(vertices, 0)[np.newaxis, :]
 
-# ---- start
-obj = {}
-camera = {}
-### face in reality: ~18cm height/width. set 180 = 18cm. image size: 256 x 256
-scale_init = 180/(np.max(vertices[:,1]) - np.min(vertices[:,1])) # scale face model to real size
-#print(np.max(vertices[:,1]) - np.min(vertices[:,1]))
+        # save folder
+        save_folder = f"results/intensity_distro/light_source/x{source[0]}y{source[1]}z{source[2]}"
+        if not os.path.exists(save_folder):
+            os.mkdir(save_folder)
+        options = '-delay 10 -loop 0 -layers optimize' # gif options. need ImageMagick installed.
 
-## 1. fix camera model(stadard camera& orth proj). change obj position.
-camera['proj_type'] = 'orthographic'
-camera['eye'] = [0, 0, 250]
-# scale
-for factor in np.arange(0.5, 1.2, 0.1):
-	obj['s'] = scale_init*factor
-	obj['angles'] = [0, 0, 0]
-	obj['t'] = [0, 0, 0]
-	image = transform_test(vertices, obj, camera, source) 
-	io.imsave('{}/1_1_{:.2f}.jpg'.format(save_folder, factor), image, quality=100)
+        # ---- start
+        obj = {}
+        camera = {}
+        ### face in reality: ~18cm height/width. set 180 = 18cm. image size: 256 x 256
+        scale_init = 180/(np.max(vertices[:,1]) - np.min(vertices[:,1])) # scale face model to real size
+        #print(np.max(vertices[:,1]) - np.min(vertices[:,1]))
 
-# angles
-for i in range(3):
-	for angle in np.arange(-50, 51, 10):
-		obj['s'] = scale_init
-		obj['angles'] = [0, 0, 0]
-		obj['angles'][i] = angle
-		obj['t'] = [0, 0, 0]
-		image = transform_test(vertices, obj, camera, source) 
-		io.imsave('{}/1_2_{}_{}.jpg'.format(save_folder, i, angle), image, quality=100)
+        ## 1. fix camera model(stadard camera& orth proj). change obj position.
+        camera['proj_type'] = 'orthographic'
+        camera['eye'] = [0, 0, 250]
+        # scale
+        for factor in np.arange(0.5, 1.2, 0.1):
+                obj['s'] = scale_init*factor
+                obj['angles'] = [0, 0, 0]
+                obj['t'] = [0, 0, 0]
+                image = transform_test(vertices, obj, camera, source) 
+                io.imsave('{}/1_1_{:.2f}.jpg'.format(save_folder, factor), image, quality=100)
 
-## 2. fix obj position(center=[0,0,0], front pose). change camera position&direction, using perspective projection(fovy fixed)
-obj['s'] = scale_init
-obj['angles'] = [0, 0, 0]
-obj['t'] = [0, 0, 0]
-# obj: center at [0,0,0]. size:200
+        # angles
+        for i in range(3):
+                for angle in np.arange(-50, 51, 10):
+                        obj['s'] = scale_init
+                        obj['angles'] = [0, 0, 0]
+                        obj['angles'][i] = angle
+                        obj['t'] = [0, 0, 0]
+                        image = transform_test(vertices, obj, camera, source) 
+                        io.imsave('{}/1_2_{}_{}.jpg'.format(save_folder, i, angle), image, quality=100)
 
-camera['proj_type'] = 'perspective'
-camera['at'] = [0, 0, 0]
-camera['near'] = 1000
-camera['far'] = -100
-# eye position
-camera['fovy'] = 30
-camera['up'] = [0, 1, 0] #
-# z-axis: eye from far to near, looking at the center of face
-for p in np.arange(500, 250-1, -40): # 0.5m->0.25m
-	camera['eye'] = [0, 0, p]  # stay in front of face
-	image = transform_test(vertices, obj, camera, source) 
-	io.imsave('{}/2_eye_1_{}.jpg'.format(save_folder, 1000-p), image, quality=100)
+        ## 2. fix obj position(center=[0,0,0], front pose). change camera position&direction, using perspective projection(fovy fixed)
+        obj['s'] = scale_init
+        obj['angles'] = [0, 0, 0]
+        obj['t'] = [0, 0, 0]
+        # obj: center at [0,0,0]. size:200
 
-# y-axis: eye from down to up, looking at the center of face
-for p in np.arange(-300, 301, 60): # up 0.3m -> down 0.3m
-	camera['eye'] = [0, p, 250] # stay 0.25m far
-	image = transform_test(vertices, obj, camera, source) 
-	io.imsave('{}/2_eye_2_{}.jpg'.format(save_folder, p/6), image, quality=100)
+        camera['proj_type'] = 'perspective'
+        camera['at'] = [0, 0, 0]
+        camera['near'] = 1000
+        camera['far'] = -100
+        # eye position
+        camera['fovy'] = 30
+        camera['up'] = [0, 1, 0] #
+        # z-axis: eye from far to near, looking at the center of face
+        for p in np.arange(500, 250-1, -40): # 0.5m->0.25m
+                camera['eye'] = [0, 0, p]  # stay in front of face
+                image = transform_test(vertices, obj, camera, source) 
+                io.imsave('{}/2_eye_1_{}.jpg'.format(save_folder, 1000-p), image, quality=100)
 
-# x-axis: eye from left to right, looking at the center of face
-for p in np.arange(-300, 301, 60): # left 0.3m -> right 0.3m
-	camera['eye'] = [p, 0, 250] # stay 0.25m far
-	image = transform_test(vertices, obj, camera, source) 
-	io.imsave('{}/2_eye_3_{}.jpg'.format(save_folder, -p/6), image, quality=100)
+        # y-axis: eye from down to up, looking at the center of face
+        for p in np.arange(-300, 301, 60): # up 0.3m -> down 0.3m
+                camera['eye'] = [0, p, 250] # stay 0.25m far
+                image = transform_test(vertices, obj, camera, source) 
+                io.imsave('{}/2_eye_2_{}.jpg'.format(save_folder, p/6), image, quality=100)
 
-# up direction
-camera['eye'] = [0, 0, 250] # stay in front
-for p in np.arange(-50, 51, 10):
-	world_up = np.array([0, 1, 0]) # default direction
-	z = np.deg2rad(p)
-	Rz=np.array([[math.cos(z), -math.sin(z), 0],
-                 [math.sin(z),  math.cos(z), 0],
-                 [     0,       0, 1]])
-	up = Rz.dot(world_up[:, np.newaxis]) # rotate up direction
-	# note that: rotating up direction is opposite to rotating obj
-	# just imagine: rotating camera 20 degree clockwise, is equal to keeping camera fixed and rotating obj 20 degree anticlockwise.
-	camera['up'] = np.squeeze(up)
-	image = transform_test(vertices, obj, camera, source) 
-	io.imsave('{}/2_eye_4_{}.jpg'.format(save_folder, -p), image, quality=100)
+        # x-axis: eye from left to right, looking at the center of face
+        for p in np.arange(-300, 301, 60): # left 0.3m -> right 0.3m
+                camera['eye'] = [p, 0, 250] # stay 0.25m far
+                image = transform_test(vertices, obj, camera, source) 
+                io.imsave('{}/2_eye_3_{}.jpg'.format(save_folder, -p/6), image, quality=100)
 
-# -- delete jpg files
-print('gifs have been generated, now delete jpgs')
-# subprocess.call('rm {}/*.jpg'.format(save_folder), shell=True)
+        # up direction
+        camera['eye'] = [0, 0, 250] # stay in front
+        for p in np.arange(-50, 51, 10):
+                world_up = np.array([0, 1, 0]) # default direction
+                z = np.deg2rad(p)
+                Rz=np.array([[math.cos(z), -math.sin(z), 0],
+                         [math.sin(z),  math.cos(z), 0],
+                         [     0,       0, 1]])
+                up = Rz.dot(world_up[:, np.newaxis]) # rotate up direction
+                # note that: rotating up direction is opposite to rotating obj
+                # just imagine: rotating camera 20 degree clockwise, is equal to keeping camera fixed and rotating obj 20 degree anticlockwise.
+                camera['up'] = np.squeeze(up)
+                image = transform_test(vertices, obj, camera, source) 
+                io.imsave('{}/2_eye_4_{}.jpg'.format(save_folder, -p), image, quality=100)
+
+        # -- delete jpg files
+        print('gifs have been generated, now delete jpgs')
+        # subprocess.call('rm {}/*.jpg'.format(save_folder), shell=True)B
